e99fff40d518abe66b562e253d97608191b633e5
webdesignchick <strawberryelle85@gmail.com>
2014-04-12 11:35:02 -0700

    first commit new project

--- assignment-1/index.php
+++ assignment-1/index.php
@@ -1,3 +1,17 @@
-<!doctype html>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
-Hello world!
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
