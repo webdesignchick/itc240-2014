82c45d570055e92ce96f93ac2e91134554f55189
webdesignchick <strawberryelle85@gmail.com>
2014-04-13 20:04:50 -0700

    Assignment 1
    index, css, header, nav, footer

---
+++ assignment-1/include/head.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $page_title ?></title>
+        <meta name="robots" content="noindex,nofollow" />
+        <link href="css/etitc240.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <header>
+            
+            <h1>
+                Welcome
+            </h1>
+            <p>My name is Ellen Tackett.  </p>
+            <p>I am the Web Design Chick.</p>
+            
+                
+        
+        </header>
+        <nav>
+            <ul>
+                <li> <a href="index.php">Home </a></li>
+                <li> <a href="#"> Assignment 2</a></li>
+                <li> <a href="#"> Assignment 3</a></li>
+            </ul>
+        </nav>
+    <main>
--- assignment-1/index.php
+++ assignment-1/index.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
+       <?php include 'include/head.php';
+       $page_title = "Home";
+
+       ?>
+
+                <p> I am working towards a Web Design Certificate.  The more time I spend in these classes the more I think I may go for the Web Developer Certificate as well. </p>
+                <p> In the course of my studies I have learned a lot of CSS and HTML.  I was nervous about working on a page without a template to start but as I worked on this page I realized that I have retained more knowledge then I thought.  I did have to look up a couple of CSS syntaxes such as the semicolon in the hover tag and the mailto for the A tag in the email link for my footer.</p>
+                <p>  I love living in Seattle! My husband and I relocated to Seattle two years ago. I love the outdoors and I used to mountain bike and hike a lot before my surgery last year. I also used to do hair but my body and the doctor told me it wasn't such a good idea anymore. I love movies and reading. You will never find my Kindle far from hand and Saturday night is not complete without a few explosions from the latest action flick.</p>
+
+        
+            <?php include 'include/footer.php';
         ?>
-    </body>
-</html>
