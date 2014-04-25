f8f3ca7c9d288dd571cde3a4da7a2c7083671e79
webdesignchick <strawberryelle85@gmail.com>
2014-04-24 19:05:23 -0700

    rename and update

--- inclassweek/424index.php
+++ inclassweek/424index.php
@@ -1,8 +1,21 @@
+<!doctype html>
+<html>
+    <head>
+    </head>
+    <body>
+        <br><br><br><br>
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+?>
+        
+        
+
+
+
+
+
+
+
+</body>
+</html>
