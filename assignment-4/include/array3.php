30bb3ac1137382a2af9aad6fd2769551c630ec75
Ellen T. <strawberryelle85@gmail.com>
2014-05-15 18:49:10 -0700

    Assignment 4 first commit

---
+++ assignment-4/include/array3.php
@@ -0,0 +1,15 @@
+<?php 
+
+$comicHero = [
+    ["name" => "Tony Stark", "alias" => "Iron Man",  "origin" => "1963", "girl" => "Pepper Potts", "fbPage" => "https://www.facebook.com/ironman", "marvelPage" => "http://marvel.com/universe/Iron_Man_(Anthony_Stark)"],
+    ["name" => "Steve Rodgers", "alias" => "Captain America",  "origin" => "1941", "girl" => "Sharon Carter", "fbPage" => "https://www.facebook.com/CaptainAmerica", "marvelPage" => "http://marvel.com/universe/Captain_America"],
+    ["name" => "Bruce Banner", "alias" => "Hulk", "origin" => "1962", "girl" => "Betty Ross", "fbPage" => "https://www.facebook.com/Hulk", "marvelPage" => "http://marvel.com/universe/Hulk_(Bruce_Banner)"],    
+    ["name" => "Thor Odinson", "alias" => "Thor", "origin" => "1951", "girl" => "Sif", "fbPage" => "https://www.facebook.com/Thor", "marvelPage" => "http://marvel.com/universe/Thor"],
+    ["name" => "Peter Parker", "alias" => "Spiderman", "origin" => "1962", "girl" => "Gwen Stacy", "fbPage" => "https://www.facebook.com/SpiderManDVD", "marvelPage" => "http://marvel.com/universe/Spider-Man_(Peter_Parker)" ],
+    ["name" => "Clint Barton", "alias" => "Hawkeye", "origin" => "1964", "girl" => "Barbara 'Bobbi' Barton (Mockingbird)", "fbPage" => "https://www.facebook.com/pages/Hawkeye/138343949523928", "marvelPage" => "http://marvel.com/universe/Ronin_(Clint_Barton)" ],
+    ["name" => "Natalia 'Natalia 'Alianovna Romanova", "alias" => "Black Widow", "origin" => "1964","girl" => "Alexi Shostakov (Red Guardian, estranged husband)", "fbPage" => "https://www.facebook.com/pages/Black-Widow/138592782836116", "marvelPage" => "http://marvel.com/universe/Black_Widow_(Natasha_Romanova)"],
+    
+    ];
+
+
+?>
---
+++ assignment-4/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Assignment 4 via FTP</title>
+    </head>
+    <body>
+        <?php
+        include 'include/array3.php';
+        include 'include/password.php';
+        
+        $mysql = new mysqli( "localhost", "etacke01", $mysql_password, "etacke01");
+        
+        $result = $mysql->query('SELECT * FROM comicHero ORDER BY origin ASC, type ASC;');
+       
+        
+        
+        
+       
+                 ?>
+        <table>
+            <?php
+            foreach ($result as $row) {
+                ?>
+            <tr>
+                <td><?=$row["type"] ?>
+                <td><?=$row["origin"]?>
+           
+             <?php
+            }
+            
+                ?>
+        </table>
+    </body>
+</html>
