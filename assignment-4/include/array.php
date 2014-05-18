619a11358189e001a360f379194174afe410c0bf
Ellen T. <strawberryelle85@gmail.com>
2014-05-18 09:56:06 -0700

    Assignment 4 Table Start
    Wrote Data
    Insert not working yet

---
+++ assignment-4/include/array.php
@@ -0,0 +1,10 @@
+<?php
+INSERT INTO comicHero (name, alias, origin, girl, archnemesis, id) 
+        VALUES  (Tony Stark, Iron Man, 1963, Mandarin, Pepper Pots, 1),
+                (Steve Rodgers, Captain America, 1941, Red Skull, Sharon Carter, 2),
+                (Bruce Banner, Hulk, 1962, Tyrannus, Betty Ross, 3),
+                (Thor Odinson, Thor, 1951, Loki, Sif, 4),
+                (Peter Parker, Spiderman, 1962, Green Goblin, Gwen Stacy, 5),
+                (Clint Barton, Hawkeye, 1964, Assassin, Barbara 'Bobbi' Barton: Mockingbird, 6),
+                (Natalia Alianovna Romanova, Black Widow, 1964, Alexi Shostakov: Red Guardian, 7);
+
--- assignment-4/index.php
+++ assignment-4/index.php
@@ -11,14 +11,25 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        include 'include/array3.php';
+        include 'include/array.php';
         include 'include/password.php';
         
         $mysql = new mysqli( "localhost", "etacke01", $mysql_password, "etacke01");
         
-        $result = $mysql->query('SELECT * FROM comicHero ORDER BY origin ASC, type ASC;');
-       
-       
+        
+        $result = $mysql->query('SELECT * FROM comicHero;');
+      
+        
+    INSERT INTO comicHero (name, alias, origin, girl, archnemesis) 
+        VALUES  (Tony Stark, Iron Man, 1963, Mandarin, Pepper Pots, 1),
+                (Steve Rodgers, Captain America, 1941, Red Skull, Sharon Carter, 2),
+                (Bruce Banner, Hulk, 1962, Tyrannus, Betty Ross, 3),
+                (Thor Odinson, Thor, 1951, Loki, Sif, 4),
+                (Peter Parker, Spiderman, 1962, Green Goblin, Gwen Stacy, 5),
+                (Clint Barton, Hawkeye, 1964, Assassin, Barbara 'Bobbi' Barton: Mockingbird, 6),
+                (Natalia Alianovna Romanova, Black Widow, 1964, Alexi Shostakov: Red Guardian, 7);
+
+
                  ?>
         <table>
             <?php
