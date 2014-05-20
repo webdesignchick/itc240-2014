25ef2b211b5a7d9593434d1d16f76d039cc8590b
Ellen T. <strawberryelle85@gmail.com>
2014-05-19 18:35:15 -0700

    Assignment 4
    With Errors need help keep getting a warning on bind-param

--- assignment-4/index.php
+++ assignment-4/index.php
@@ -7,42 +7,73 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Assignment 4 via FTP</title>
+        <title>Assignment 4</title>
     </head>
     <body>
         <?php
-        include 'include/array.php';
+      
         include 'include/password.php';
         
         $mysql = new mysqli( "localhost", "etacke01", $mysql_password, "etacke01");
         
+        /* check connection */
+        //if (mysqli_connect_errno()) {
+        //printf("Connect failed: %s\n", mysqli_connect_error());
+        //exit();}
         
-        $result = $mysql->query('SELECT * FROM comicHero;');
+               
+       $prepared = $mysql->prepare("INSERT INTO comicHero (name, alias, origin, girl, archnemesis, id)
+             VALUES 
+                    ('Steve Rodgers', 'Captain America', 1941, 'Red Skull', 'Sharon Carter', '?'),
+                    ('Bruce Banner', 'Hulk', 1962, 'Tyrannus', 'Betty Ross', '?'),
+                    ('Thor Odinson', 'Thor', 1951, 'Loki', 'Sif', '?'),
+                    ('Peter Parker', 'Spiderman', 1962, 'Green Goblin', 'Gwen Stacy', '?'),
+                    ('Clint Barton', 'Hawkeye', 1964, 'Assassin', 'Barbara Barton: Mockingbird', '?'),
+                    ('Natalia Alianovna Romanova', 'Black Widow', 1964, 'Black Spectre', 'Alexi Shostakov: Red Guardian', '?');");       
+   
+       $prepared->bind_param("ssissi", 
+               $_REQUEST["name"],
+               $_REQUEST["alias"],
+               $_REQUEST["origin"],
+               $_REQUEST["girl"],
+               $_REQUEST["archnemesis"], 
+               $_REQUEST["id"]);
+               
+       $prepared->execute();
+         $result = $mysql->query('SELECT * FROM comicHero ORDER BY origin ASC, type ASC');
+       $mysql->close();
+       
       
+       
         
-    INSERT INTO comicHero (name, alias, origin, girl, archnemesis) 
-        VALUES  (Tony Stark, Iron Man, 1963, Mandarin, Pepper Pots, 1),
-                (Steve Rodgers, Captain America, 1941, Red Skull, Sharon Carter, 2),
-                (Bruce Banner, Hulk, 1962, Tyrannus, Betty Ross, 3),
-                (Thor Odinson, Thor, 1951, Loki, Sif, 4),
-                (Peter Parker, Spiderman, 1962, Green Goblin, Gwen Stacy, 5),
-                (Clint Barton, Hawkeye, 1964, Assassin, Barbara 'Bobbi' Barton: Mockingbird, 6),
-                (Natalia Alianovna Romanova, Black Widow, 1964, Alexi Shostakov: Red Guardian, 7);
-
-
+    
+//$result = $mysql->query('SELECT * FROM comicHero()');
+ //print_r($result);
+       foreach ( $result as $show ){
+           
+       }
+   
                  ?>
         <table>
-            <?php
-            foreach ($result as $row) {
-                ?>
-            <tr>
-                <td><?=$row["type"] ?>
-                <td><?=$row["origin"]?>
            
-             <?php
-            }
-            
-                ?>
+            <tr>
+                <th>Name</th>
+                <th>Alias</th>
+                <th>Origin</th>
+                <th>Relationship</th>
+                <th>Nemesis</th>
+                <th>Id</th>
+            </tr>
+            <tr>
+            <td><?=$show["name"]?></td>
+            <td><?=$show["alias"]?></td>
+            <td><?=$show["origin"]?></td>
+            <td><?=$show["girl"]?></td>
+            <td><?=$show["archnemesis"]?></td>
+            <td><?=$show["id"]?></td>
+
+            </tr>
         </table>
+
     </body>
 </html>
