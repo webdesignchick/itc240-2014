8c9591967ef558fe4a4c587d0ce64689ec5d2c95
Ellen T. <strawberryelle85@gmail.com>
2014-05-20 20:31:31 -0700

    Forgot to Date In-Class for 05/20/2014
    In-Class work

--- in-class/index.php
+++ in-class/index.php
@@ -11,15 +11,44 @@
     include 'password.php';
     
     $prepared = $mysql->prepare('SELECT * FROM tracker_food');
+    $prepared->execute();
+    $results = $prepared->get_result();
+    
+        ?>
+    <h3>What you ate:</h3>
+    <table>
+        <tr>
+            <th>Calories</th>
+            <th>Type</th>
+            <th>Entry No.</th>
+        </tr>
+        <tr>
+<?php
+    foreach ($results as $row) {
+?>
+            <td><?= htmlentities($row["id"])?></td>
+            <td><?= htmlentities($row["calories"])?></td>
+            <td><?= htmlentities($row["type"])?></td>
+            
+        </tr>   
+        
+    </table>
     
     
+    <?php 
+    }
+    $sumQuery = $mysql->prepare ('SELECT sum(calories) AS Sum FROM tracker_food');
+    $sumQuery->execute();
+    $sumResult = $sumQuery->get_result();
     
-    
+    $sum = $sumResult->fetch_array ();
     
     
     
     ?>
-
+    <div>Your total caloric intake is.
+    <?= $sum["sum"]?>
+    </div>
     
     </body>
 </html>
